aa550c71de15eefd3ad3afabfeb1d3f00709288d
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-11-20 20:05:41 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,5 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
-	end
-
 	get '/hello' do
 		erb :hello
 	end
@@ -15,4 +11,5 @@ class App < Sinatra::Base
 	get '/date' do
 		erb :date
 	end
+
 end
